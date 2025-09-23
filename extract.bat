fcc945820c704bdff35bccacba1d7952b5ea6632
Clement TH Chung <cthchung@cthchung.com>
2025-09-23 10:16:17 -0400

    Batch update

--- extract.bat
+++ extract.bat
@@ -1,9 +1,11 @@
 @echo off
 if [%1]==[] goto usage
-.\App\utils\innounp\innounp.exe -q -x -dApp %1 {code_GetDestDir}\*
+echo Extracting files from installer...
+.\App\utils\innounp\innounp.exe -q -x -dtemp %1 {code_GetDestDir}\* >nul 2>&1
 rem move /y ".\App\{code_GetDestDir}" ".\App\cursor"
-xcopy /S /Y ".\App\{code_GetDestDir}" ".\App\cursor"
-rd /S /Q ".\App\{code_GetDestDir}"
+xcopy /S /Y /I ".\temp\{code_GetDestDir}" ".\App\cursor" >nul 2>&1
+echo Cleanup...
+rd /S /Q ".\temp"
 goto end
 
 
